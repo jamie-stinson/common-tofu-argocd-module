b7191e9939e3a70d18de0fccc9779fe184c4b1f0
Jamie <32368259+jamie-stinson@users.noreply.github.com>
2024-12-06 23:05:57 +0000

    feat: include break glass user credentials

--- release.tf
+++ release.tf
@@ -4,8 +4,16 @@ resource "helm_release" "argocd" {
   chart            = "argo-cd"
   repository       = "https://argoproj.github.io/argo-helm"
   version          = "7.7.7"
-  values           = [var.argocd.values]
-  create_namespace = true
+  values = [
+    var.argocd.values,
+    yamlencode({
+      configs = {
+        secret = {
+          argocdServerAdminPassword = bcrypt(random_password.argocd_admin_password.result)
+        }
+      }
+    })
+  ]  create_namespace = true
   timeout          = 600
   wait             = true
 }
