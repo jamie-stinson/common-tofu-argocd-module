8d060c59c5d81c7de8281ce9acece9fbfc7528d6
Jamie <32368259+jamie-stinson@users.noreply.github.com>
2025-01-03 14:04:19 +0000

    feat: refactor (#34)

--- variables.tf
+++ variables.tf
@@ -4,6 +4,25 @@ variable "argocd" {
   })
 }
 
+variable "cloudflare_api_token" {
+  type        = string
+  default     = "empty"
+  sensitive = true
+}
+
+variable "cloudflare_domain" {
+  type        = string
+  default     = "empty"
+  sensitive = true
+
+}
+
+variable "cloudflare_email" {
+  type        = string
+  default     = "empty"
+  sensitive = true
+}
+
 variable "environment" {
   type = string
 }
