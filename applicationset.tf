a4742be84661070431377533d7969282ff68d551
Jamie <32368259+jamie-stinson@users.noreply.github.com>
2025-01-05 15:23:03 +0000

    feat: apply cloudflare api token to ingress-certificate app only (#40)

--- applicationset.tf
+++ applicationset.tf
@@ -1,6 +1,7 @@
 resource "kubectl_manifest" "applicationset" {
     sensitive_fields = [
-        "spec.templatePatch"
+        "spec.templatePatch",
+        "spec.template.spec.sources"
     ]
     yaml_body = <<YAML
 apiVersion: argoproj.io/v1alpha1
@@ -26,6 +27,18 @@ spec:
       project: "{{ index .path.segments 3 }}"
       revisionHistoryLimit: 3
       sources:
+        - repoURL: "ghcr.io/jamie-stinson/common-helm-library"
+          chart: "common-helm-library"
+          targetRevision: "1.*.*"
+          helm:
+            releaseName: "{{ index .path.segments 1 }}"
+            valueFiles:
+              - "$values/global-values.yaml"
+              - "$values/charts/{{ index .path.segments 1 }}/values.yaml"
+              - "$values/charts/{{ index .path.segments 1 }}/environments/{{ index .path.segments 3 }}/values.yaml"
+            valuesObject:
+              ingress:
+                domain: "${var.cloudflare_domain}"
         - repoURL: https://github.com/jamie-stinson/helm-system-monorepo.git
           targetRevision: HEAD
           ref: values
@@ -50,6 +63,9 @@ spec:
           - RespectIgnoreDifferences=false
           - ApplyOutOfSyncOnly=false
   templatePatch: |
+    metadata:
+      name: "{{ index .path.segments 1 }}"
+            {{ if eq (index .path.segments 1) "ingress-certificate" }}
     spec:
       sources:
         - repoURL: "ghcr.io/jamie-stinson/common-helm-library"
@@ -61,7 +77,6 @@ spec:
               - "$values/global-values.yaml"
               - "$values/charts/{{ index .path.segments 1 }}/values.yaml"
               - "$values/charts/{{ index .path.segments 1 }}/environments/{{ index .path.segments 3 }}/values.yaml"
-            {{ if eq (index .path.segments 1) "ingress-certificate" }}
             valuesObject:
               wildcardCertificate:
                 apiToken: "${var.cloudflare_api_token}"
@@ -69,6 +84,10 @@ spec:
                 email: "${var.cloudflare_email}"
               ingress:
                 domain: "${var.cloudflare_domain}"
+        - repoURL: https://github.com/jamie-stinson/helm-system-monorepo.git
+          targetRevision: HEAD
+          ref: values
+          path: charts/{{ index .path.segments 1 }}/crds
             {{- end }}
 YAML
   depends_on  = [
